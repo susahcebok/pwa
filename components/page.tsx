8d65f238fd818d37e5a51224a4534dd8928d14df
Ferry Ayunda <115649359+susahcebok@users.noreply.github.com>
2022-11-09 14:08:31 +0700

    Update page.tsx

--- components/page.tsx
+++ components/page.tsx
@@ -11,7 +11,7 @@ const Page = ({ title, children }: Props) => (
 	<>
 		{title ? (
 			<Head>
-				<title>Rice Bowl | {title}</title>
+				<title>Ferry Ayunda | {title}</title>
 			</Head>
 		) : null}
 
